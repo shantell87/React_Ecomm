a8cefd2d122911b837185a736248197b63b69586
Shantell Davis <shantell87@gmail.com>
2020-05-12 21:45:19 -0700

    added app.js; created and tested express server

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+
+app.get('/', (req, res) => {
+    res.send('hello from node');
+});
+
+const port = process.env.PORT || 8000
+
+app.listen(port, () => {
+    console.log(`Sever is running on port ${port}`)
+});
+
