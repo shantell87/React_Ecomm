8b4da918b239d83af879bd68fafb43a7559edb34
Shantell Davis <shantell87@gmail.com>
2020-05-20 14:15:23 -0700

    Created product controller, model, and route. Added products using Postman succesfully.

--- app.js
+++ app.js
@@ -4,12 +4,15 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const expressValidator = require('express-validator');
+
 require('dotenv').config();
 
 //import routes
 const authRoutes = require('./routes/auth');
 const userRoutes = require('./routes/user');
 const categoryRoutes = require('./routes/category');
+const productRoutes = require('./routes/product');
+
 
 // app
 const app = express();
@@ -33,6 +36,7 @@ app.use(expressValidator());
 app.use('/api', authRoutes);
 app.use('/api', userRoutes);
 app.use('/api', categoryRoutes);
+app.use('/api', productRoutes);
 
 const port = process.env.PORT || 8000;
 
