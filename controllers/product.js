33c31b482ccdc7e54b4ead9f4755cd4707adde6f
Shantell Davis <shantell87@gmail.com>
2020-05-20 14:25:56 -0700

    added product field validation requirements controllers/product.js

--- controllers/product.js
+++ controllers/product.js
@@ -14,9 +14,26 @@ exports.create = (req, res) => {
                 error: "Image could not be uploaded"
             });
         }
+        //check for all fields
+        const {name, description, price, category, quantity, shipping} = fields
+        
+        if(!name || !description || !price || !category || !quantity || !shipping) 
+        {
+            return res.status(400).json({
+                error: "All fields are required"
+            });
+        }
+
         let product = new Product(fields);
 
         if (files.photo) {
+            //console.log("Files Photo:", files.photo)
+            if (files.photo.size > 1000000) {
+                return res.status(400).json({
+                    error: "Image should be less than 1mb in size"
+                });
+            }
+
             product.photo.data = fs.readFileSync(files.photo.path);
             product.photo.contentType = files.photo.type;
         }
