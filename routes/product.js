b8b3746d017e43c72401b88d32a485d99dd523e4
Shantell Davis <shantell87@gmail.com>
2020-05-24 20:09:26 -0700

    search category by id. Category update, delete, and get all. Product by sell and arrival. Retrieve product based on request type. List products by search param.

--- routes/product.js
+++ routes/product.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const router = express.Router();
 
-const { create, productById, read, remove, update } = require("../controllers/product");
+const { create, productById, read, remove, update, list, listRelated, listCategories, listBySearch } = require("../controllers/product");
 const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
 const { userById } = require('../controllers/user');
 
 router.get("/product/:productId", read);
-router.post("/product/create/:userId", requireSignin, isAuth, isAdmin, create);
-router.delete("/product/:productId/:userId", requireSignin, isAuth, isAdmin, remove);
+router.post("/product/create/:userId", 
+requireSignin, isAuth, isAdmin, create);
+router.delete("/product/:productId/:userId", 
+requireSignin, isAuth, isAdmin, remove);
 router.put(
     "/product/:productId/:userId",
     requireSignin,
@@ -15,6 +17,11 @@ router.put(
     isAdmin,
     update
 );
+
+router.get("/products", list);
+router.get("/products/related/:productId", listRelated);
+router.get("/products/categories", listCategories);
+router.post("/products/by/search", listBySearch);
 
 
 router.param("userId", userById);
