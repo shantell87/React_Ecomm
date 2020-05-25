28f690ebc93e6d913d6ca555debf8f03b4b917f8
Shantell Davis <shantell87@gmail.com>
2020-05-24 20:28:00 -0700

    send product photo search. Fetch user profile and update data.

--- routes/product.js
+++ routes/product.js
@@ -1,15 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
-const { create, productById, read, remove, update, list, listRelated, listCategories, listBySearch } = require("../controllers/product");
+const { create, productById, read, remove, update, list, listRelated, listCategories, listBySearch, photo } = require("../controllers/product");
 const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
 const { userById } = require('../controllers/user');
 
 router.get("/product/:productId", read);
-router.post("/product/create/:userId", 
-requireSignin, isAuth, isAdmin, create);
-router.delete("/product/:productId/:userId", 
-requireSignin, isAuth, isAdmin, remove);
+router.post("/product/create/:userId", requireSignin, isAuth, isAdmin, create);
+router.delete("/product/:productId/:userId", requireSignin, isAuth, isAdmin, remove);
 router.put(
     "/product/:productId/:userId",
     requireSignin,
@@ -22,6 +20,7 @@ router.get("/products", list);
 router.get("/products/related/:productId", listRelated);
 router.get("/products/categories", listCategories);
 router.post("/products/by/search", listBySearch);
+router.get("/product/photo/:productId", photo);
 
 
 router.param("userId", userById);
