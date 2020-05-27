e5bde663af3d77eb6e21e983680a66d2828cb201
Shantell Davis <shantell87@gmail.com>
2020-05-26 17:17:07 -0700

    added config file for API address. Added auth index file for sign/signup components. Created signin/signup forms and tested backend connection.

--- client/src/user/Signup.js
+++ client/src/user/Signup.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
+import { Link } from "react-router-dom";
 import Layout from '../core/Layout';
-import { API } from '../config';
+import {signup} from '../auth/index';
 
 
 const Signup = () => {
@@ -12,32 +13,29 @@ const Signup = () => {
         success: false
     });
 
-    const { name, email, password } = values;
+    const { name, email, password, success, error } = values;
 
     const handleChange = name => event => {
         setValues({ ...values, error: false, [name]: event.target.value });
     };
 
-    const signup = user => {
-        fetch(`${API}/signup`, {
-            method: "POST",
-            headers: {
-                Accept: "application/json",
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(user)
-        })
-            .then(response => {
-                return response.json();
-            })
-            .catch(err => {
-                console.log(err);
-            });
-    };
-
     const clickSubmit = event => {
         event.preventDefault();
-        signup({ name, email, password });
+        setValues({ ...values, error: false });
+        signup({ name, email, password }).then(data => {
+            if (data.error) {
+                setValues({ ...values, error: data.error, success: false });
+            } else {
+                setValues({
+                    ...values,
+                    name: "",
+                    email: "",
+                    password: "",
+                    error: "",
+                    success: true
+                });
+            }
+        });
     };
 
     const signUpForm = () => (
@@ -48,6 +46,7 @@ const Signup = () => {
                     onChange={handleChange("name")}
                     type="text"
                     className="form-control"
+                    value={name}
                 />
             </div>
 
@@ -57,6 +56,7 @@ const Signup = () => {
                     onChange={handleChange("email")}
                     type="email"
                     className="form-control"
+                    value={email}
                 />
             </div>
 
@@ -66,6 +66,7 @@ const Signup = () => {
                     onChange={handleChange("password")}
                     type="password"
                     className="form-control"
+                    value={password}
                 />
             </div>
             <button onClick={clickSubmit} className="btn btn-primary">
@@ -74,14 +75,33 @@ const Signup = () => {
         </form>
     );
 
+    const showError = () => (
+        <div
+            className="alert alert-danger"
+            style={{ display: error ? "" : "none" }}
+        >
+            {error}
+        </div>
+    );
+
+    const showSuccess = () => (
+        <div
+            className="alert alert-info"
+            style={{ display: success ? "" : "none" }}
+        >
+            New account is created. Please <Link to="/signin">Signin</Link>
+        </div>
+    );
+
     return (
         <Layout
             title="Signup"
             description="Signup to Node React E-commerce App"
             className="container col-md-8 offset-md-2"
         >
+            {showSuccess()}
+            {showError()}
             {signUpForm()}
-            {JSON.stringify(values)}
         </Layout>
     );
 };
