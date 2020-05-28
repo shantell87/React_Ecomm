db47d8848a94bb4820d5207cb10059704dacff44
Shantell Davis <shantell87@gmail.com>
2020-05-28 13:31:04 -0700

    added user PrivateRoute and UserDashboard files

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -3,13 +3,17 @@ import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import Signup from './user/Signup';
 import Signin from './user/Signin';
 import Home from './core/Home';
+import PrivateRoute from './auth/PrivateRoute';
+import Dashboard from './user/UserDashboard';
 
 const Routes = () => {
-    return (<BrowserRouter>
+    return (
+    <BrowserRouter>
         <Switch>
             <Route path='/' exact component={Home}/>
             <Route path='/signin' exact component={Signin}/>
             <Route path='/signup' exact component={Signup}/>
+            <PrivateRoute path="/dashboard" exact component={Dashboard} />
         </Switch>
     </BrowserRouter>
     );
--- client/src/core/Menu.js
+++ client/src/core/Menu.js
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { Link, withRouter } from "react-router-dom";
-import { signout } from '../auth/index';
+import { signout, isAuthenticated } from '../auth/index';
 
 const isActive = (history, path) => {
     if (history.location.pathname === path) {
@@ -16,46 +16,52 @@ const Menu = ({ history }) => (
             <li className="nav-item">
                 <Link
                     className="nav-link"
-                    style={isActive(history, "/")}
-                    to="/"
+                    style={isActive(history, "/dashboard")}
+                    to="/dashboard"
                 >
                     Home
                 </Link>
             </li>
 
-            <li className="nav-item">
-                <Link
-                    className="nav-link"
-                    style={isActive(history, "/signin")}
-                    to="/signin"
-                >
-                    Signin
-                </Link>
-            </li>
+            {!isAuthenticated() && (
+                <Fragment>
+                    <li className="nav-item">
+                        <Link
+                            className="nav-link"
+                            style={isActive(history, "/signin")}
+                            to="/signin"
+                        >
+                            Signin
+                        </Link>
+                    </li>
 
-            <li className="nav-item">
-                <Link
-                    className="nav-link"
-                    style={isActive(history, "/signup")}
-                    to="/signup"
-                >
-                    Signup
-                </Link>
-            </li>
+                    <li className="nav-item">
+                        <Link
+                            className="nav-link"
+                            style={isActive(history, "/signup")}
+                            to="/signup"
+                        >
+                            Signup
+                        </Link>
+                    </li>
+                </Fragment>
+            )}
 
-            <li className="nav-item">
-                <span
-                    className="nav-link"
-                    style={{ cursor: "pointer", color: "#ffffff" }}
-                    onClick={() =>
-                        signout(() => {
-                            history.push("/");
-                        })
-                    }
-                >
-                    Signout
-                </span>
-            </li>
+            {isAuthenticated() && (
+                <li className="nav-item">
+                    <span
+                        className="nav-link"
+                        style={{ cursor: "pointer", color: "#ffffff" }}
+                        onClick={() =>
+                            signout(() => {
+                                history.push("/");
+                            })
+                        }
+                    >
+                        Signout
+                    </span>
+                </li>
+            )}
         </ul>
     </div>
 );
