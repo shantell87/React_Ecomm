10a171b2fae5caac82211bbd523e5f383ce7daf1
Shantell Davis <shantell87@gmail.com>
2020-06-03 18:34:05 -0700

    updated server.js

--- client/server.js
+++ client/server.js
@@ -10,10 +10,6 @@ app.get('*', function (req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.get('/ping', function (req, res) {
-    return res.send('pong');
-});
-
 const PORT = process.env.PORT || 3000;
 
 mongoose.connect(
