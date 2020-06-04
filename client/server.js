e1eff01ff8b0e8ee47974b510654f1c08367cd65
Shantell Davis <shantell87@gmail.com>
2020-06-03 20:46:00 -0700

    updated app.js; deployment troubleshoot

--- client/server.js
+++ client/server.js
@@ -6,19 +6,11 @@ const app = express();
 app.use(compression());
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 const PORT = process.env.PORT || 3000;
-
-mongoose.connect(
-    process.env.MONGODB_URI || "mongodb://<dbuser>:<dbpassword>@ds161890.mlab.com:61890/heroku_5m2hprn8",
-    {
-        useCreateIndex: true,
-        useNewUrlParser: true
-    }
-);
 
 app.listen(PORT, () => {
     console.log(`App is running on port ${PORT}`);
