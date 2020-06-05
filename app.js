f97fc24af72f897f84a2ad360528c8aa40009f9a
Shantell Davis <shantell87@gmail.com>
2020-06-05 13:53:14 -0700

    troubleshooting heroku deploy

--- app.js
+++ app.js
@@ -38,8 +38,7 @@ app.use('/api', braintreeRoutes);
 app.use("/api", orderRoutes);
 
 //db
-mongoose
-.connect(process.env.DATABASE, {
+mongoose.connect(process.env.DATABASE, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true
@@ -49,12 +48,13 @@ mongoose
 
 const port = process.env.PORT || 8000;
 
-// Serve up static assets
-app.use(express.static(path.join(__dirname, "client", "build")))
 
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-});
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+}
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
