cb6932090b6f4bcdacea5317ee7eb35fe4406250
Shantell Davis <shantell87@gmail.com>
2020-06-04 20:39:39 -0700

    heroku troubleshooting

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ const app = express();
 
 // Serve up static assets
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));
+    app.use(express.static(path.join(__dirname, 'client/build')));
 }
 
 //db
