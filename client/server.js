6c0077c0614b5c4fa8548af224798625862acd03
Shantell Davis <shantell87@gmail.com>
2020-06-16 19:21:38 -0700

    update server.js; troubleshooting deployment

--- client/server.js
+++ client/server.js
@@ -10,12 +10,8 @@ if (process.env.NODE_ENV === 'production') {
 }
 // app.use(express.static(path.join(__dirname, 'client/build')));
 
-// app.get('*', function(req, res) {
-//     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-// });
-
 app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, '../../client/build/index.html'));
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
 
 const PORT = process.env.PORT || 3000;
