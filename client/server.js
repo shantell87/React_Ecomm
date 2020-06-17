1b7caf031267d398d67833f27864cca69cc1bdc2
Shantell Davis <shantell87@gmail.com>
2020-06-16 22:58:28 -0700

    troubleshoot heroku

--- client/server.js
+++ client/server.js
@@ -5,10 +5,10 @@ const app = express();
 
 app.use(compression());
 
-if (process.env.NODE_ENV === 'production') {
-	app.use(express.static('client/build'));
-}
-// app.use(express.static(path.join(__dirname, 'client/build')));
+// if (process.env.NODE_ENV === 'production') {
+// 	app.use(express.static('client/build'));
+// }
+app.use('*', express.static(path.join(__dirname, "client", "build")))
 
 app.get('*', function(req, res) {
     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
