e58473d2a1663badbc1926cf780e19056fa9d2d5
Shantell Davis <shantell87@gmail.com>
2020-06-16 19:18:47 -0700

    update server.js; troubleshooting deployment

--- client/server.js
+++ client/server.js
@@ -4,10 +4,18 @@ const path = require('path');
 const app = express();
 
 app.use(compression());
-app.use(express.static(path.join(__dirname, 'build')));
+
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'));
+}
+// app.use(express.static(path.join(__dirname, 'client/build')));
+
+// app.get('*', function(req, res) {
+//     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+// });
 
 app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, '../../client/build/index.html'));
 });
 
 const PORT = process.env.PORT || 3000;
