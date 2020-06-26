32f40bd8fc4f93f39d7f7c7d5f87ded64f639a6b
Shantell Davis <shantell87@gmail.com>
2020-06-25 23:59:58 -0700

    troubleshooting  mongo db for heroku

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.use('/api', braintreeRoutes);
 app.use("/api", orderRoutes);
 
 //db
-mongoose.connect(process.env.DATABASE, {
+mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
@@ -44,13 +44,6 @@ mongoose.connect(process.env.DATABASE, {
 })
     .then(() => console.log('DB Connected'));
 
-mongoose.connect(process.env.MONGODB_URI, {
-        useNewUrlParser: true,
-        useCreateIndex: true,
-        useUnifiedTopology: true,
-        useFindAndModify:false
-    })
-        .then(() => console.log('MLAB Connected'));
 
 if(process.env.NODE_ENV === 'production'){
     app.use(express.static('client/build'));
